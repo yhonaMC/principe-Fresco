d86d9c69812c69ca487c3a17a06fe927eb5b719c
Yhonaiker Moncada <yhonaiker1@gmail.com>
2021-08-31 10:23:46 -0700

    cambios

--- js/index.js
+++ js/index.js
@@ -1,59 +1,32 @@
-const items = document.getElementById('items')
-const templateCard = document.getElementById('template-card').content
+const divImagen = document.getElementById('product');
 
-const fragment = document.createDocumentFragment()
-let carrito = {}
 
-// Eventos
-// El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
-document.addEventListener('DOMContentLoaded', e => { fetchData() });
-cards.addEventListener('click', e => { addCarrito(e) });
-items.addEventListener('click', e => { btnAumentarDisminuir(e) })
+const verProductos = async () => {
+  divImagen.innerHTML = "";
+  const response = await fetch('http://localhost:4000/productos');
+  const data = await response.json();
 
-// Traer productos
-const fetchData = async () => {
-    const res = await fetch('../bd.json');
-    const data = await res.json()
-    // console.log(data)
-    pintarCards(data)
+  data.forEach(productos => {
+    const { categoria, title, precio, descripcion, thumbnailUrl } = productos;
+    divImagen.innerHTML += `
+     <div class="col">
+     <div class="card">
+       <a href="">
+         <img src="${thumbnailUrl.img2}">
+       </a>            
+       <div class="card-body">
+         <h5 class="card-title">${title}</h5>
+         <p class="card-text">${categoria}</p>
+         <p class="card-text">${descripcion}</p>
+         <p class="card-text">$ ${precio}</p>
+         <button class="btn btn-dark">Comprar</button>
+       </div>
+     </div>
+ </div>    
+     `;
+  });
 }
 
-// Pintar productos
-const pintarCards = data => {
-    data.forEach(item => {
-        templateCard.querySelector('h5').textContent = item.title
-        templateCard.querySelector('p').textContent = item.precio
-        templateCard.querySelector('button').dataset.id = item.id
-        const clone = templateCard.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-    cards.appendChild(fragment)
-}
+verProductos();
 
-// Agregar al carrito
-const addCarrito = e => {
-    if (e.target.classList.contains('btn-dark')) {
-        // console.log(e.target.dataset.id)
-        // console.log(e.target.parentElement)
-        setCarrito(e.target.parentElement)
-    }
-    e.stopPropagation()
-}
 
-const setCarrito = item => {
-    // console.log(item)
-    const producto = {
-        title: item.querySelector('h5').textContent,
-        precio: item.querySelector('p').textContent,
-        id: item.querySelector('button').dataset.id,
-        cantidad: 1
-    }
-    // console.log(producto)
-    if (carrito.hasOwnProperty(producto.id)) {
-        producto.cantidad = carrito[producto.id].cantidad + 1
-    }
-
-    carrito[producto.id] = { ...producto }
-    
-    pintarCarrito()
-}
