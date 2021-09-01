d1df14227fbe9213b7e8a721c258faa59e479ad5
yhonaMC <yhonaiker1@gmail.com>
2021-08-31 18:09:22 -0700

    cambios new

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
 const divImagen = document.getElementById('product');
-
+const btnMujeres = document.getElementById('hombres');
+const btnHombres = document.getElementById('mujeres');
 
 const verProductos = async () => {
- divImagen.innerHTML="";
- const response = await fetch('http://localhost:4000/productos');
- const data = await response.json();
+  divImagen.innerHTML = "";
+  const response = await fetch('http://localhost:4000/productos');
+  const data = await response.json();
 
- data.forEach(productos => {
-     const {categoria,title,precio,descripcion,thumbnailUrl} = productos;
-     divImagen.innerHTML +=`
-     <div class="col">
-     <div class="card">
-       <a href="">
-         <img src="${thumbnailUrl.img2}">
-       </a>            
-       <div class="card-body">
-         <h5 class="card-title">${title}</h5>
-         <p class="card-text">${categoria}</p>
-         <p class="card-text">${descripcion}</p>
-         <p class="card-text">$ ${precio}</p>
-         <button class="btn btn-dark">Comprar</button>
-       </div>
-     </div>
- </div>    
-     `;
- });
+  data.forEach(productos => {
+    const { categoria, title, precio, descripcion, thumbnailUrl } = productos;
+    divImagen.innerHTML += `
+      <div class="col-md-4 center">
+      <div class="card">
+        <a href="">
+          <img src="${thumbnailUrl.img1}">
+        </a>            
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${categoria}</p>
+          <p class="card-text">${descripcion}</p>
+          <p class="card-text">$ ${precio}</p>
+          <button class="btn btn-dark">Comprar</button>
+        </div>
+      </div>
+  </div>    
+      `;
+
+  });
 }
 
 verProductos();
